e801591c8b8799dfd779fcd6be0dc25e01247f88
RandomPythonProgrammer <jc2006.12345@outlook.com>
2021-04-29 16:10:15 -0500

    Wip google search works now, bing search is in the process of development.

--- Image_Search.py
+++ Image_Search.py
@@ -3,22 +3,35 @@ from selenium import webdriver
 import time
 import requests
 import Search_Tags
+import platform
 import os
 import base64
+import ast
+
+
+if platform.system() == "Windows":
+    driver_path = 'chrome_driver/chromedriver.exe'
+elif platform.system() == "Darwin":
+    driver_path = 'chrome_driver/chromedriver'
 
 
 class Image:
-    def __init__(self, link):
+    def __init__(self, link, source):
         self.link = link
-        try:
-            self.data = requests.get(self.link).content
+        self.source = source
+        self.type = None
+        if source == "google":
             try:
-                self.type = os.path.split(self.link)[1]
-            except OSError:
+                self.data = requests.get(self.link).content
+            except requests.exceptions.InvalidSchema:
+                self.data = base64.b64decode(self.link.split(",")[-1])
+                self.type = self.link.split(",")[0].split("/")[-1].split(";")[0]
+            if self.type is None:
+                print("defaulted")
                 self.type = "jpeg"
-        except requests.exceptions.InvalidSchema:
-            self.data = base64.b64decode(self.link.split(",")[-1])
-            self.type = self.link.split(",")[0].split("/")[-1].split(";")[0]
+        elif source == "bing":
+            self.data = requests.get(self.link).content
+            self.type = os.path.split(self.link)[1]
 
     def save(self, filename):
         with open(f"{filename}.{self.type}", "wb") as image_file:
@@ -26,47 +39,51 @@ class Image:
 
 
 def bing_search_image(search_term: str, number_of_images: int, *tags):
-    """Returns a list of images from the search results from bing, no gif support, WIP not completely working"""
+    """Returns a list of images from the search results from bing, sometimes defaults to jpegs"""
     options = webdriver.ChromeOptions()
+    #options.add_argument("--headless")
     dr = webdriver.Chrome(
-        executable_path='chromedriver.exe',
+        driver_path,
         options=options,
 
     )
     dr.get(f"https://www.bing.com/images/search?q={search_term}{''.join(tags)}")
-    time.sleep(1)
+    time.sleep(30)
     soup = BeautifulSoup(dr.page_source, "html.parser")
     dr.close()
-    image_list = soup.find_all("img", class_="mimg")
+    image_list = soup.find_all("a", class_="iusc")
     return_images = []
     for picture in image_list[:number_of_images]:
-        return_images.append(Image(picture.get("src")))
+        dictionary = picture.get("m")
+        return_images.append(Image(ast.literal_eval(dictionary)["murl"], "bing"))
 
     return return_images
 
 
 def google_search_image(search_term: str, number_of_images: int, *tags):
-    """Returns a list of images from the search results from google, no gif support"""
+    """Returns a list of images from the search results from google, sometimes defaults to jpegs"""
     options = webdriver.ChromeOptions()
+    options.add_argument('disable-infobars')
+    options.add_experimental_option("useAutomationExtension", False)
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_argument("--headless")
+
     dr = webdriver.Chrome(
-        executable_path='chromedriver.exe',
+        executable_path="chrome_driver/chromedriver",
         options=options,
     )
     dr.get(f"http://www.google.com/search?q={search_term}&tbm=isch{''.join(tags)}")
-    time.sleep(1)
+    time.sleep(3)
     soup = BeautifulSoup(dr.page_source, "html.parser")
     dr.close()
-    images = soup.find_all("img", class_="n3VNCb")
-    for image in images:
-        print(image.link)
+    images = soup.find_all("img", class_="rg_i Q4LuWd")
     return_images = []
     for picture in images[:number_of_images]:
-        return_images.append(Image(picture.get("src")))
+        return_images.append(Image(picture.get("src"), "google"))
 
     return return_images
 
 
-images = bing_search_image("dog", 10)
+images = bing_search_image("dog", 10, Search_Tags.Bing_Png)
 for image in images:
     image.save(f"saves/{images.index(image)}")
