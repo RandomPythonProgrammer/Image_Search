a83bc776e985688352ad116dec99b7b2ad21b932
RandomPythonProgrammer <74526339+RandomPythonProgrammer@users.noreply.github.com>
2021-05-11 22:46:27 -0500

    More Changes

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     long_description="Idk just a random python image search thing, it uses selenium and stuff.",
     install_requires=['selenium', 'bs4', 'requests'],
     keywords="Image Search",
-    packages="Image_Search"
+    packages=["Image_Search"]
 )
