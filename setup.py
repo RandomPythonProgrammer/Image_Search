26040aabd79e3e9456adcf060a889901df29c573
RandomPythonProgrammer <74526339+RandomPythonProgrammer@users.noreply.github.com>
2021-05-11 22:24:17 -0500

    More Changes

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='python-image-search',
+    name='python-image_search',
     version="1.0",
     url='https://github.com/RandomPythonProgrammer/python-image-search',
-    author='RandomPythonProgramer',
+    author='RandomPythonProgrammer',
     author_email="jhc3628@gmail.com",
     description='Simple image search',
+    long_description="Idk just a random python image search thing, it uses selenium and stuff.",
     install_requires=['selenium', 'bs4', 'requests'],
     keywords="Image Search",
     scripts=["Image_Search.py"]
